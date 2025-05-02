d173f6f4cc6ef8ad22fffae5862cf70f11941a18
Pavel Shliatski <pavelshlatcki1@gmail.com>
2025-05-02 15:10:37 +0300

    feat: some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
     return (
       <div className="header">
         <div className="header_container">
-        <img src={ToDoIcon} alt="" style={{width: 40, height: 40, paddingBottom: 5}} draggable={false}/>
-        <h1>TODO</h1>
+           <a href="."><img src={ToDoIcon} alt="" style={{width: 40, height: 40, paddingBottom: 9, }} draggable={false}/>
+          <h1>TODO</h1></a>
         </div>
       </div>
     );
